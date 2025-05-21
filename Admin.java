3d681c9baa96879b0b620b320fffb4207d5d9bbb
Rushdi M.R. <IT24101405@my.sliit.lk>
2025-05-21 15:17:11 +0530

    Update Admin.java
    admin extends user
    super() keyword to pass values to parent attributes

--- Admin.java
+++ Admin.java
@@ -1,52 +1,18 @@
 package com.example.propertymanagement.model;
 
-public class Admin {
-    private String id;
-    private String name;
-    private String contactInfo;
 
+
+public class Admin extends User {   //extends from user(parent class)
     public Admin() {
+        super();     //pass data to parent attributes
     }
 
-    public Admin(String id, String name, String contactInfo) {
-        this.id = id;
-        this.name = name;
-        this.contactInfo = contactInfo;
-    }
-
-    // Getters and Setters
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getContactInfo() {
-        return contactInfo;
-    }
-
-    public void setContactInfo(String contactInfo) {
-        this.contactInfo = contactInfo;
+    public Admin(Long id, String username, String password, String email) {
+        super();
     }
 
     @Override
-    public String toString() {
-        return "Admin{" +
-                "id='" + id + '\'' +
-                ", name='" + name + '\'' +
-                ", contactInfo='" + contactInfo + '\'' +
-                '}';
+    public String getRole() {
+        return "ADMIN";
     }
 }
-
